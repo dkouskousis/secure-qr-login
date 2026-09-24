bffd785ab5fb3ee02f593541ad182c91b652fc05
Dimitris Kouskousis <web@dkouskousis.gr>
2026-09-25 00:27:23 +0300

    Add Secure QR Login integration files

---
+++ custom_components/secure_qr_login/config_flow.py
@@ -0,0 +1,16 @@
+"""Config flow for Secure QR Login."""
+
+from homeassistant import config_entries
+
+from .const import DOMAIN, NAME
+
+
+class SecureQrLoginConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        await self.async_set_unique_id(DOMAIN)
+        self._abort_if_unique_id_configured()
+        if user_input is not None:
+            return self.async_create_entry(title=NAME, data={})
+        return self.async_show_form(step_id="user")
